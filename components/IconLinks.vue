c2b42040e88f91c60d4c02ef13de560794d2dc6f
jesper.svensson@linux.com <jesper.svensson@linux.com>
2022-07-01 20:50:55 +0200

    info# adding more info with external links to my social media.

---
+++ components/IconLinks.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <br />
+    <span>
+      <!-- GitHub -->
+      <a href="https://github.com/Jesperson" target="_blank">
+        <b-icon icon="github" size="is-large"></b-icon>
+      </a>
+      <!-- LinkedIn -->
+      <a
+        href="https://www.linkedin.com/in/jesper-svensson-262235151/"
+        target="_blank"
+      >
+        <b-icon icon="linkedin" size="is-large"> </b-icon>
+      </a>
+      <!-- Instagram -->
+      <a
+        href="https://www.instagram.com/jespersonisnotavailable/"
+        target="_blank"
+      >
+        <b-icon icon="instagram" size="is-large"></b-icon>
+      </a>
+    </span>
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IconLinks',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
