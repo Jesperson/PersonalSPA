bd3c6c68f23007843bdcd67b3ae34484633ee8a8
jesper.svensson@linux.com <jesper.svensson@linux.com>
2022-07-02 00:12:50 +0200

    fix/feature: fixed a few grammatical errors and trying to add a script to only build for dev and master/main.

--- pages/about.vue
+++ pages/about.vue
@@ -3,19 +3,21 @@
     <div class="content is-mobile">
       <h4 class="title">A Bit About Me</h4>
       <p class="test">
-        A software developer from Sweden, graduated 13/5-22, primarily studied
-        and used C# and .NET for the majority of this latest educational period,
-        this page is an exception as I wanted to try out Nuxt JS. Some
-        experience in Python, C++, React/React-Native and other languages.
+        Software developer from Sweden, graduated in may of this year (2022),
+        primarily studied and used C#/.NET for the majority of this latest
+        educational period, this page is obviously written in TypeScript as I
+        wanted to try out Nuxt JS. Some experience in Python, C++,
+        Type/JavaScript and a few other languages.
+
         <br />
         Currently working for
         <a href="https://www.visma.se/draftit/" target="_blank">
           Visma DraftIt
         </a>
-        as a full-stack systems developer working with React and C#.
+        as a full-stack systems developer working mainly with React and C#.
         <br />
         All opinions are obviously my own and do in no way necessarily reflect
-        them.
+        my employer.
         <br />
         <IconLinks></IconLinks>
       </p>
@@ -30,6 +32,3 @@ export default {
   components: { IconLinks },
 }
 </script>
-
-<style scoped>
-</style>
