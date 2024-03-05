30edd5ed4afbb85687bd8ebe0adfc70c586029e6
Jesper Svensson <jesper.svensson@linux.com>
2024-03-05 17:57:33 +0100

    Edited information about myself

--- pages/about.vue
+++ pages/about.vue
@@ -3,14 +3,14 @@
     <div class="content is-mobile">
       <h4 class="title">A Bit About Me</h4>
       <p class="test">
-        A software developer from Sweden, graduated 13/5-22, primarily studied
+        A software developer from Sweden, graduated in 2022, primarily studied
         and used C# and .NET for the majority of this latest educational period,
         this page is an exception as I wanted to try out Nuxt JS. Some
         experience in Python, C++, React/React-Native and other languages.
         <br />
         Currently working for
-        <a href="https://www.visma.se/draftit/" target="_blank">
-          Visma DraftIt
+        <a href="https://www.mcdonalds.com/se/sv-se.html" target="_blank">
+          McDonalds
         </a>
         as a software engineer. All opinions are obviously my own and do in no
         way necessarily reflect them.
